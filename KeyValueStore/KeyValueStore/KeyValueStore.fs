c79b8ca812abcf08ccf756d59a75c09c62239f6d
= <tim.p.barker@googlemail.com>
2015-03-01 20:14:35 +0000

    Moves KeyValueStore into this repo

---
+++ KeyValueStore/KeyValueStore/KeyValueStore.fs
@@ -0,0 +1,39 @@
+﻿module KeyValueStore
+
+type StoreOperation<'K, 'V when 'K : comparison> = 
+    | Set of 'K * 'V
+    | Get of 'K * 'V option AsyncReplyChannel
+    | Delete of 'K
+    | Count of int AsyncReplyChannel
+    | Clear
+
+[<Sealed>]
+type KeyValueStore<'K, 'V when 'K : comparison>() = 
+    let cts = new System.Threading.CancellationTokenSource()
+    
+    let store = 
+        MailboxProcessor.Start((fun inbox -> 
+                               let rec loop table = 
+                                   async { 
+                                       let! operation = inbox.Receive()
+                                       match operation with
+                                       | Set(key, value) -> return! loop (Map.add key value table)
+                                       | Get(key, chan) -> 
+                                           chan.Reply(Map.tryFind key table)
+                                           return! loop table
+                                       | Delete key -> return! loop (Map.remove key table)
+                                       | Clear -> return! loop Map.empty
+                                       | Count chan -> 
+                                           chan.Reply(table.Count)
+                                           return! loop table
+                                   }
+                               loop Map.empty), cts.Token)
+    
+    interface System.IDisposable with
+        member this.Dispose() = cts.Cancel()
+    
+    member this.Set (key : 'K) (value : 'V) = store.Post(Set(key, value))
+    member this.Get key = store.PostAndAsyncReply(fun chan -> Get(key, chan))
+    member this.Delete key = store.Post(Delete key)
+    member this.Clear() = store.Post Clear
+    member this.Count = store.PostAndAsyncReply Count
---
+++ KeyValueStore/KeyValueStore/KeyValueStore.fsi
@@ -0,0 +1,11 @@
+﻿module KeyValueStore
+
+[<Sealed>]
+type KeyValueStore<'K, 'V when 'K : comparison> = 
+    interface System.IDisposable
+    new : unit -> KeyValueStore<'K, 'V>
+    member Set : 'K -> 'V -> unit
+    member Get : 'K -> 'V option Async
+    member Delete : 'K -> unit
+    member Clear : unit -> unit
+    member Count : int Async
