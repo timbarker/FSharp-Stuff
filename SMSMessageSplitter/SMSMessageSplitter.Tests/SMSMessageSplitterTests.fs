c3fd32034d68a442325acd3301d7d6d358c66572
= <tim.p.barker@googlemail.com>
2015-03-03 22:19:33 +0000

    Cleanup in SMSMessageSplitter

--- SMSMessageSplitter/SMSMessageSplitter.Tests/SMSMessageSplitterTests.fs
+++ SMSMessageSplitter/SMSMessageSplitter.Tests/SMSMessageSplitterTests.fs
@@ -13,12 +13,20 @@ type ``A GSM Single part splitter``() =
         splits |> should haveLength 1
         splits |> should equal [ String.replicate 160 "a" ]
     
-    [<Test>]
-    member x.``Should handle escape characters``() = 
-        let message = String.replicate 80 "€"
+    [<TestCase("|")>]
+    [<TestCase("^")>]
+    [<TestCase("€")>]
+    [<TestCase("{")>]
+    [<TestCase("}")>]
+    [<TestCase("[")>]
+    [<TestCase("~")>]
+    [<TestCase("]")>]
+    [<TestCase("\\")>]
+    member x.``Should handle extended characters`` (extendedChar) = 
+        let message = String.replicate 80 extendedChar
         let splits = SMSMessageSplitter.splitGSM message
         splits |> should haveLength 1
-        splits |> should equal [ String.replicate 80 "€" ]
+        splits |> should equal [ String.replicate 80 extendedChar ]
 
 [<TestFixture>]
 type ``A GSM multipart splitter``() = 
@@ -31,16 +39,24 @@ type ``A GSM multipart splitter``() =
         splits |> should equal [ String.replicate 153 "a"
                                  "a" ]
     
-    [<Test>]
-    member x.``Should handle escapte characters``() = 
-        let message = String.replicate 77 "€"
+    [<TestCase("|")>]
+    [<TestCase("^")>]
+    [<TestCase("€")>]
+    [<TestCase("{")>]
+    [<TestCase("}")>]
+    [<TestCase("[")>]
+    [<TestCase("~")>]
+    [<TestCase("]")>]
+    [<TestCase("\\")>]
+    member x.``Should handle extended characters`` (extendedChar) = 
+        let message = String.replicate 77 extendedChar
         let splits = SMSMessageSplitter.splitGSM message
         splits |> should haveLength 2
-        splits |> should equal [ String.replicate 76 "€"
-                                 "€" ]
-
+        splits |> should equal [ String.replicate 76 extendedChar
+                                 extendedChar ]
+    
     [<Test>]
-    member x.``Should not let last character be escape character``() =
+    member x.``Should not let last character be escape character``() = 
         let message = (String.replicate 152 "a") + "€" + (String.replicate 10 "a")
         let splits = SMSMessageSplitter.splitGSM message
         splits |> should haveLength 2
@@ -48,93 +64,101 @@ type ``A GSM multipart splitter``() =
                                  "€aaaaaaaaaa" ]
 
 [<TestFixture>]
-type ``A Unicode single part splitter``() =
+type ``A Unicode single part splitter``() = 
     
     [<Test>]
-    member x.``Should handle single code unit characters``() =
+    member x.``Should handle single code unit characters``() = 
         let message = String.replicate 70 "á"
         let splits = SMSMessageSplitter.splitUnicode message
         splits |> should haveLength 1
         splits |> should equal [ String.replicate 70 "á" ]
-
+    
     [<Test>]
-    member x.``Should handle double code unit characters``() =
+    member x.``Should handle double code unit characters``() = 
         let message = String.replicate 35 "\uD83D\uDC7D"
         let splits = SMSMessageSplitter.splitUnicode message
         splits |> should haveLength 1
         splits |> should equal [ String.replicate 35 "\uD83D\uDC7D" ]
 
 [<TestFixture>]
-type ``A Unicode multi part splitter``() =
-
+type ``A Unicode multi part splitter``() = 
+    
     [<Test>]
     member x.``Should handle single code unit characters``() = 
         let message = String.replicate 71 "á"
         let splits = SMSMessageSplitter.splitUnicode message
         splits |> should haveLength 2
         splits |> should equal [ String.replicate 67 "á"
-                                 String.replicate 4 "á"]
-
+                                 String.replicate 4 "á" ]
+    
     [<Test>]
     member x.``Should handle double code unit characters``() = 
         let message = String.replicate 71 "á"
         let splits = SMSMessageSplitter.splitUnicode message
         splits |> should haveLength 2
         splits |> should equal [ String.replicate 67 "á"
-                                 String.replicate 4 "á"]
-
+                                 String.replicate 4 "á" ]
+    
     [<Test>]
-    member x.``Should not let last character be first part of surrogate pair``() =
+    member x.``Should not split surrogate pair at end of part``() = 
         let message = (String.replicate 66 "á") + "\uD83D\uDC7D" + (String.replicate 10 "á")
         let splits = SMSMessageSplitter.splitUnicode message
         splits |> should haveLength 2
-        splits |> should equal [ String.replicate 66 "á"; 
-                                        "\uD83D\uDC7Dáááááááááá"]
+        splits |> should equal [ String.replicate 66 "á"
+                                 "\uD83D\uDC7Dáááááááááá" ]
 
 [<TestFixture>]
-type ``A GSM truncator``() =
-
+type ``A GSM truncator``() = 
+    
     [<Test>]
-    member x.``Should truncate standard chracters correct length``() =
+    member x.``Should truncate standard chracters correct length``() = 
         let message = String.replicate 165 "a"
         let truncated = SMSMessageSplitter.truncateGSM message
         truncated |> should haveLength 160
-        truncated |> should equal (String.replicate 160 "a") 
-
-    [<Test>]
-    member x.``Should truncate extended chracters to correct length``() =
-        let message = String.replicate 85 "€"
+        truncated |> should equal (String.replicate 160 "a")
+    
+    [<TestCase("|")>]
+    [<TestCase("^")>]
+    [<TestCase("€")>]
+    [<TestCase("{")>]
+    [<TestCase("}")>]
+    [<TestCase("[")>]
+    [<TestCase("~")>]
+    [<TestCase("]")>]
+    [<TestCase("\\")>]
+    member x.``Should truncate extended chracters to correct length`` (extendedChar) = 
+        let message = String.replicate 85 extendedChar
         let truncated = SMSMessageSplitter.truncateGSM message
         truncated |> should haveLength 80
-        truncated |> should equal (String.replicate 80 "€") 
-
+        truncated |> should equal (String.replicate 80 extendedChar)
+    
     [<Test>]
-    member x.``Should respect escape characters at end of string``() =
+    member x.``Should not let escape character be at end of string``() = 
         let message = (String.replicate 159 "a") + "€aaaa"
         let truncated = SMSMessageSplitter.truncateGSM message
         truncated |> should haveLength 159
-        truncated |> should equal (String.replicate 159 "a") 
+        truncated |> should equal (String.replicate 159 "a")
 
 [<TestFixture>]
-type ``A Unicode truncator``() =
+type ``A Unicode truncator``() = 
     
     [<Test>]
-    member x.``Should truncate single code unit chararcters to correct length``() =
+    member x.``Should truncate single code unit chararcters to correct length``() = 
         let message = String.replicate 75 "á"
         let truncated = SMSMessageSplitter.truncateUnicode message
         truncated |> should haveLength 70
-        truncated |> should equal (String.replicate 70 "á") 
-
+        truncated |> should equal (String.replicate 70 "á")
+    
     [<Test>]
-    member x.``Should truncate double code unit chararcters to correct length``() =
+    member x.``Should truncate double code unit chararcters to correct length``() = 
         let message = String.replicate 40 "\uD83D\uDC7D"
         let truncated = SMSMessageSplitter.truncateUnicode message
         truncated |> should haveLength 70
-        truncated |> should equal (String.replicate 35 "\uD83D\uDC7D") 
-
+        truncated |> should equal (String.replicate 35 "\uD83D\uDC7D")
+    
     [<Test>]
-    member x.``Should repect surrogate pairs at end of string``() =
+    member x.``Should not split surrogate pair at end of string``() = 
         let message = String.replicate 69 "á" + "\uD83D\uDC7D"
         let truncated = SMSMessageSplitter.truncateUnicode message
         truncated |> should haveLength 69
-        truncated |> should equal (String.replicate 69 "á") 
+        truncated |> should equal (String.replicate 69 "á")
--- SMSMessageSplitter/SMSMessageSplitter/SMSMessageSplitter.fs
+++ SMSMessageSplitter/SMSMessageSplitter/SMSMessageSplitter.fs
@@ -9,27 +9,27 @@ let getCharacterInfoGSM char =
 
 let getCharacterInfoUnicode char = 
     match char with
-    | c when Char.IsHighSurrogate(c) -> (c, 4)
-    | c when Char.IsLowSurrogate(c) -> (c, 0)
-    | c -> (c, 2)
+    | _ when Char.IsHighSurrogate(char) -> (char, 4)
+    | _ when Char.IsLowSurrogate(char) -> (char, 0)
+    | _ -> (char, 2)
 
-let splitter partLength (state : (char list * int) list) (char : char * int) = 
-    match state, char with
+let splitter partLength state charInfo = 
+    match state, charInfo with
     | (part, partSz) :: parts, (char, charSz) when partSz + charSz <= partLength -> 
         (char :: part, partSz + charSz) :: parts
     | parts, (char, charSz) -> ([ char ], charSz) :: parts
 
 let toString (arr : char array) = new String(arr)
 
-let split getCharInfo partSize singlePartSize (message: string) = 
+let split getCharInfo partSize singlePartSize (message : string) = 
     let split = 
         message
         |> Seq.map getCharInfo
         |> Seq.fold (splitter partSize) [ ([], 0) ]
         |> List.rev
     
-    let effectiveTotalLength = split |> List.sumBy snd
-    if effectiveTotalLength = singlePartSize then 
+    let totalLength = split |> List.sumBy snd
+    if totalLength = singlePartSize then 
         [ split
           |> Seq.map fst
           |> Seq.map List.rev
@@ -45,6 +45,5 @@ let split getCharInfo partSize singlePartSize (message: string) =
 
 let splitGSM = split getCharacterInfoGSM 153 160
 let splitUnicode = split getCharacterInfoUnicode 134 140
-
 let truncateGSM = split getCharacterInfoGSM 160 160 >> List.head
 let truncateUnicode = split getCharacterInfoUnicode 140 140 >> List.head
